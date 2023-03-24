505f05492ef2937714c90f05e1678913a8671603
Darya Krinitsyna <krinitsyna.darya.98@mail.ru>
2023-03-24 14:58:56 +0300

    fix: replace jwt secret for verify

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,13 +2,15 @@ const jwt = require('jsonwebtoken');
 
 const UnauthorizedError = require('../errors/unauthorized-err');
 
+const { JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   // верифицируем токен
   let payload;
   try {
     // попытаемся верифицировать токен
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     // отправим ошибку, если не получилось
     next(new UnauthorizedError('Необходима авторизация'));
